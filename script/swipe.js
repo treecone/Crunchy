f9395ab2f1f83f194e90c226879bcacd1adf3d1f
Robert <42982719+Zytion@users.noreply.github.com>
2020-02-08 20:49:42 -0500

    finished swipe, working on linking

--- script/swipe.js
+++ script/swipe.js
@@ -1,84 +1,89 @@
-$(document).ready(function() {
+//Created by https://codepen.io/suez/pen/MaeVBy?editors=0010
 
-  var animating = false;
-  var cardsCounter = 0;
-  var numOfCards = 6;
-  var decisionVal = 80;
-  var pullDeltaX = 0;
-  var deg = 0;
-  var $card, $cardReject, $cardLike;
+$(document).ready(function () {
 
-  function pullChange() {
-    animating = true;
-    deg = pullDeltaX / 10;
-    $card.css("transform", "translateX("+ pullDeltaX +"px) rotate("+ deg +"deg)");
+    var animating = false;
+    var cardsCounter = 0;
+    var numOfCards = 6;
+    var decisionVal = 80;
+    var pullDeltaX = 0;
+    var deg = 0;
+    var $card, $cardReject, $cardLike;
+    var titles = document.querySelectorAll('div.food_titles');
 
-    var opacity = pullDeltaX / 100;
-    var rejectOpacity = (opacity >= 0) ? 0 : Math.abs(opacity);
-    var likeOpacity = (opacity <= 0) ? 0 : opacity;
-    $cardReject.css("opacity", rejectOpacity);
-    $cardLike.css("opacity", likeOpacity);
-  };
 
-  function release() {
+    function pullChange() {
+        animating = true;
+        deg = pullDeltaX / 10;
+        $card.css("transform", "translateX(" + pullDeltaX + "px) rotate(" + deg + "deg)");
 
-    if (pullDeltaX >= decisionVal) {
-      $card.addClass("to-right");
-      saveFood();
-    } else if (pullDeltaX <= -decisionVal) {
-      $card.addClass("to-left");
-    }
+        var opacity = pullDeltaX / 100;
+        var rejectOpacity = (opacity >= 0) ? 0 : Math.abs(opacity);
+        var likeOpacity = (opacity <= 0) ? 0 : opacity;
+        $cardReject.css("opacity", rejectOpacity);
+        $cardLike.css("opacity", likeOpacity);
+    };
 
-    if (Math.abs(pullDeltaX) >= decisionVal) {
-      $card.addClass("inactive");
+    function release() {
 
-      setTimeout(function() {
-        $card.addClass("below").removeClass("inactive to-left to-right");
-        cardsCounter++;
-        if (cardsCounter === numOfCards) {
-          cardsCounter = 0;
-          $(".demo__card").removeClass("below");
+        if (pullDeltaX >= decisionVal) {
+            $card.addClass("to-right");
+            saveFood();
+        } else if (pullDeltaX <= -decisionVal) {
+            $card.addClass("to-left");
         }
-      }, 300);
-    }
 
-    if (Math.abs(pullDeltaX) < decisionVal) {
-      $card.addClass("reset");
-    }
+        if (Math.abs(pullDeltaX) >= decisionVal) {
+            $card.addClass("inactive");
+            titles[cardsCounter].id = "";
+            setTimeout(function () {
+                $card.addClass("below").removeClass("inactive to-left to-right");
+                cardsCounter++;
+                $card.children('img').attr("src", "")
+                if (cardsCounter === numOfCards) {
+                    cardsCounter = 0;
+                    $(".demo__card").removeClass("below");
+                }
+                titles[cardsCounter].id = "active";
+            }, 300);
+        }
 
-    setTimeout(function() {
-      $card.attr("style", "").removeClass("reset")
-        .find(".demo__card__choice").attr("style", "");
+        if (Math.abs(pullDeltaX) < decisionVal) {
+            $card.addClass("reset");
+        }
 
-      pullDeltaX = 0;
-      animating = false;
-    }, 300);
-  };
+        setTimeout(function () {
+            $card.attr("style", "").removeClass("reset")
+                .find(".demo__card__choice").attr("style", "");
 
-  $(document).on("mousedown touchstart", ".demo__card:not(.inactive)", function(e) {
-    if (animating) return;
+            pullDeltaX = 0;
+            animating = false;
+        }, 300);
+    };
 
-    $card = $(this);
-    $cardReject = $(".demo__card__choice.m--reject", $card);
-    $cardLike = $(".demo__card__choice.m--like", $card);
-    var startX =  e.pageX || e.originalEvent.touches[0].pageX;
+    $(document).on("mousedown touchstart", ".demo__card:not(.inactive)", function (e) {
+        if (animating) return;
 
-    $(document).on("mousemove touchmove", function(e) {
-      var x = e.pageX || e.originalEvent.touches[0].pageX;
-      pullDeltaX = (x - startX);
-      if (!pullDeltaX) return;
-      pullChange();
+        $card = $(this);
+        $cardReject = $(".demo__card__choice.m--reject", $card);
+        $cardLike = $(".demo__card__choice.m--like", $card);
+        var startX = e.pageX || e.originalEvent.touches[0].pageX;
+
+        $(document).on("mousemove touchmove", function (e) {
+            var x = e.pageX || e.originalEvent.touches[0].pageX;
+            pullDeltaX = (x - startX);
+            if (!pullDeltaX) return;
+            pullChange();
+        });
+
+        $(document).on("mouseup touchend", function () {
+            $(document).off("mousemove touchmove mouseup touchend");
+            if (!pullDeltaX) return; // prevents from rapid click events
+            release();
+        });
     });
-
-    $(document).on("mouseup touchend", function() {
-      $(document).off("mousemove touchmove mouseup touchend");
-      if (!pullDeltaX) return; // prevents from rapid click events
-      release();
-    });
-  });
 });
 
-function saveFood()
-{
+function saveFood() {
     console.log("Food Saved")
 }
