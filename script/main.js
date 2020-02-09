ae001f94a85d2756373527d47ae60b9df6adc19b
Skyler Zartman <zartsky8@gmail.com>
2020-02-08 21:02:53 -0500

    OtherMenu

--- script/main.js
+++ script/main.js
@@ -1,12 +1,21 @@
 let foodWindow;
+let detailMenu;
 let menuIsDown = true;
+let detailIsDown = true;
 
 window.addEventListener('load', function()
 { 
     foodWindow = document.querySelector("div#savedFoodWindow");
+    detailMenu = document.querySelector("div#foodDetailWindow");
+    detialWindow = document.querySelector("div#foodDetailWindow");
     document.querySelector("#Saved_Button1").addEventListener("click", moveFoodWindow);
     document.querySelector("#Saved_Button2").addEventListener("click", moveFoodWindow);
-
+    let allGalleryButtons = document.querySelectorAll(".galleryImg");
+    for(let foodButton of allGalleryButtons)
+    {
+        foodButton.addEventListener("click", moveDetailWindow);
+    }
+    document.querySelector("#arrowButton").addEventListener("click", moveDetailWindow);
 })
 
 function moveFoodWindow()
@@ -22,4 +31,19 @@ function moveFoodWindow()
         foodWindow.style.transition = "transform 1s ease-in";
     }
     menuIsDown = !menuIsDown;
+}
+
+function moveDetailWindow ()
+{
+    if(detailIsDown)
+    {
+        detailMenu.style.transform = "translateY(-100%)";
+        detailMenu.style.transition = "transform 1s ease-out";
+    }
+    else
+    {
+        detailMenu.style.transform = "translateY(100%)";
+        detailMenu.style.transition = "transform 1s ease-in";
+    }
+    detailIsDown = !detailIsDown;
 }
